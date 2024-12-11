8617cc8b910e05bdce101c92218aa59a841c82b5
Blindtroop <kaluhilance4@gmail.com>
2024-12-11 15:16:21 +0300

    Fix:Added proton

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,21 @@ const Projects = () => {
 	return (
 		<ProjectsProvider>
 			<div className="container mx-auto">
+				{/* Render the grid of projects */}
 				<ProjectsGrid />
+				
+				{/* Add the View More Projects button */}
+				<div className="flex justify-center mt-8">
+					<a
+						href="https://supabase.com/dashboard/project/wbnfahnpceiemgqgcmrf/storage/buckets/fred"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300"
+						aria-label="More Projects"
+					>
+						View More Projects
+					</a>
+				</div>
 			</div>
 		</ProjectsProvider>
 	);
